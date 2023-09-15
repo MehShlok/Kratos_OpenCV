330971a1603434cf3717daebcf66848d2a1df274
ArpitPancholi <122693994+ArpitPancholi@users.noreply.github.com>
2023-09-15 22:41:04 +0530

    Update hand_detect.py , prints distance of hand and works in range of distance

--- hand_detect.py
+++ hand_detect.py
@@ -1,35 +1,47 @@
 import cv2
 import mediapipe as mp
+import math
 
-mp_drawing=mp.solutions.drawing_utils
-mp_drawing_styles=mp.solutions.drawing_styles
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 
-mphands=mp.solutions.hands
+mphands = mp.solutions.hands
 
-cap=cv2.VideoCapture(0)
-hands=mphands.Hands()
+cap = cv2.VideoCapture(0)
+hands = mphands.Hands()
 
 while True:
-    data,image=cap.read()
+    data, image = cap.read()
 
-    #Flip the image
-    image=cv2.cvtColor(cv2.flip(image,1),cv2.COLOR_BGR2RGB)
+    # Flip the image
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
 
-    #storing the results
-    results=hands.process(image)
-    image=cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+    # Storing the results
+    results = hands.process(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
                 image,
-                hand_landmarks,mphands.HAND_CONNECTIONS)
-        
-    cv2.imshow('Handtracker',image)
+                hand_landmarks, mphands.HAND_CONNECTIONS)
+
+            # Calculate the distance between thumb tip (landmark 4) and index finger tip (landmark 8)
+            thumb_tip = hand_landmarks.landmark[4]
+            index_finger_tip = hand_landmarks.landmark[8]
+            distance = math.sqrt((thumb_tip.x - index_finger_tip.x)**2 + (thumb_tip.y - index_finger_tip.y)**2)
+
+            # Display "Hand detected" with the approximate distance
+            cv2.putText(image, f"Hand detected - Distance: {distance:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            # Check if the distance is between 0.5 and 1, and print "Move"
+            if 0.20 <= distance <= 0.50:
+                cv2.putText(image, "Move", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.imshow('Handtracker', image)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+# When everything is done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-
